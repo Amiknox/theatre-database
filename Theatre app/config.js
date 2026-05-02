e26a07c301ae4b6d61f01cf39f5abe3172cf646e
Amiknox <amiknox@protonmail.com>
2026-05-02 11:47:46 +0100

    Update config.js

--- Theatre app/config.js
+++ Theatre app/config.js
@@ -1,4 +1,4 @@
 window.THEATRE_CONFIG = {
-  supabaseUrl: "",
-  supabaseAnonKey: ""
+  supabaseUrl: "https://jigqwbrfnfxlddfimecz.supabase.co",
+  supabaseAnonKey: "sb_publishable_cobxszbk6oan_iHPDp6Cvw_d8mV6KiX"
 };
